c103d8e41bbe3804f622d354250b1e1b9ae6ad32
Raquelbateman <aliciaraquel1017@gmail.com>
2024-02-01 19:48:02 -0800

    Update

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+// Have an array of different words - data.json
+// Make a counter variable to count the number of remaining turns/turns taken
+// Random generator or function to grab a word from our array
+// Create a number of underscores equal to the length of our random word
+// Keep a letter bank, to show which letters the user has already tried
+// Counter for correct guesses, so that we know if they've won the game
+// Input field for user
+// Start Button
+// Replay Button
+// Make a display field for remaining/used turns
+// Loop to display the letters in their correct positions when the user guesses a letter correctly
+
